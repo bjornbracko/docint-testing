478ad60c059f7abf8ae1b81e4b147bd5601dffd2
bjornbracko <bjorn.bracko@gmail.com>
2024-07-05 09:43:58 +0200

    updaTE

--- main.py
+++ main.py
@@ -17,43 +17,32 @@ def main():
     pdf_file = st.file_uploader("Upload a PDF file", type=["pdf"])
 
    
-    if link or pdf_file:
-        with st.spinner("Processing..."):
-            response = None
-            if pdf_file:
-                pdf_bytes = pdf_file.getvalue()
-                
-                # Encode the bytes to base64
-                b64_pdf = base64.b64encode(pdf_bytes).decode()
-                request_body = {
-                    "file": b64_pdf,
-                }
-                response = requests.post(func_url, json=request_body, params={"doc": doc})
-            elif link:
-                response = requests.get(func_url, params={"link": link, "doc": doc})
+    if st.button("Submit"):
+        if link or pdf_file and doc:
+            with st.spinner("Processing..."):
+                response = None
+                if pdf_file:
+                    pdf_bytes = pdf_file.getvalue()
+                    
+                    # Encode the bytes to base64
+                    b64_pdf = base64.b64encode(pdf_bytes).decode()
+                    request_body = {
+                        "file": b64_pdf,
+                    }
+                    response = requests.post(func_url, json=request_body, params={"doc": doc})
+                elif link:
+                    response = requests.get(func_url, params={"link": link, "doc": doc})
 
 
-        try:
-            data = json.loads(response.text)
-        except:
-            print(response)
-            data = pd.DataFrame()
-        # Display Total and Invoice Number
-        #col1, col2 = st.columns(2)
-
-        #with col1:
-        #    st.write(f"Invoice Number: {data['InvoiceNumber']}")
-            
-        #with col2:
-        #    st.write(f"Total: {data['Total']}")
-
-        # Convert the items list to a pandas DataFrame and display it
-        items_df = pd.DataFrame(data['Items'])
-        del data['Items']
-        if data != {}:
-            st.write(data)
-        st.table(items_df)
-
+            try:
+                data = json.loads(response.text) 
+                items_df = pd.DataFrame(data['Items'])
+                del data['Items']
+                if data != {}:
+                    st.write(data)
+                st.table(items_df)
+            except:
+                st.write(response)
 
 
 if __name__ == "__main__":
